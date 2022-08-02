594de35105a38d2f52636a41e7aaed16ff3d4261
sgilrutgers <109001249+sgilrutgers@users.noreply.github.com>
2022-08-02 13:15:12 -0400

    Update sender.py

--- sender.py
+++ sender.py
@@ -9,7 +9,7 @@ import os
 
 parser = argparse.ArgumentParser(description="""This is a very basic client program""")
 parser.add_argument('port', type=int, help='This is the port to connect to the server on', action='store')
-parser.add_argument('-f', type=str, help='This is the source file for the strings to reverse', default='source_strings.txt',action='store', dest='in_file')
+parser.add_argument('-f', type=str, help='This is the source file for transfer', default='test_input.txt',action='store', dest='in_file')
 
 
 args = parser.parse_args(argv[1:])
@@ -21,3 +21,4 @@ ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ss.bind((HOST, PORT))
 conn, address = ss.accept()
 
+#review class lecture for the packet sending, I believe for stop and wait we would need to send the packets line by line, and we send it to the reciever until they get it back
