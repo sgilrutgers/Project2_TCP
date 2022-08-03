2031b982e311204ac38ef57e60123bc39ec22b7b
adhamiProjects <76720190+adhamiProjects@users.noreply.github.com>
2022-08-03 10:57:33 -0400

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,24 +1,78 @@
-#basically a client that does not send strings but a whole file
-import socket
+from receiver import make_TCP_PACK,make_TCP_UNPACK
 import argparse
-from reciever import make_TCP_PACK, make_TCP_UNPACK
 from sys import argv
-import os
+import socket
+
+def pack_data(contents,start,end):
+    # print("Start:" + str(start))
+    # print("end:" + str(end))
+    # print("len:" + str(len(contents)))
+    if(start  >  len(contents)): #if start overflows
+        return 'NO CONTENT'
+    if(end > len(contents)): #if end overflows
+        return contents[start:len(contents)]
+    return contents [start:end]
 
 
 
-parser = argparse.ArgumentParser(description="""This is a very basic client program""")
-parser.add_argument('port', type=int, help='This is the port to connect to the server on', action='store')
-parser.add_argument('-f', type=str, help='This is the source file for transfer', default='test_input.txt',action='store', dest='in_file')
+def main():
+    parser=argparse.ArgumentParser(description="""This is a very basic client program""")
+    parser.add_argument('port', type=int, help='This is the port to connect to the server on',action='store')
+    parser.add_argument('server_location', type=str, help='This is the domain name or ip address of the server',action='store')
+    parser.add_argument('recv_port', type=int, help='This is the recv port', action='store')
+    args = parser.parse_args(argv[1:])
+
+    with open('input.txt') as f:
+        contents = f.read()
+        contents.encode('utf-8')
+
+    HOST = ''
+    PORT = args.port
+    RECV_PORT = args.recv_port
+    SERVER_LOCATION = args.server_location
+    SEQ_NUM = 0
+    ACK_NUM = 0
+
+    PACK_SIZE_BYTES = 1
+    PACK_START = 0
+    PACK_END = 1
+
+    receiver_addr = (SERVER_LOCATION, RECV_PORT)
+    sender_addr = ('', PORT)
+
+    sender_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    sender_sock.bind(sender_addr)
+    sender_sock.connect(receiver_addr)
+    count = 0
+
+    while count != 3:
+        packetData = pack_data(contents,PACK_START,PACK_END).encode('utf-8')
+
+        if(packetData == b'NO CONTENT'):
+            packet = make_TCP_PACK(SEQ_NUM,ACK_NUM, FIN = 1,ACK=1)
+            sender_sock.send(packet)
+            break
+
+        packet = make_TCP_PACK(SEQ_NUM, ACK_NUM,PORT, RECV_PORT) + packetData
+        # sender_sock.sendto(packet,receiver_addr)
+        sender_sock.send(packet)
+
+        ack = sender_sock.recv(128)
+        ack = make_TCP_UNPACK(ack)
+        print(ack)
+
+        ACK_NUM = int(ack['ack_number'])
+
+        count += 1
+
+        PACK_START = ACK_NUM
+        PACK_END = PACK_START + PACK_SIZE_BYTES
+        SEQ_NUM = 0
+        # ACK_NUM += 1
+        #print(ack)
+    packet = make_TCP_PACK(SEQ_NUM,ACK_NUM, FIN = 1,ACK=1)
+    sender_sock.sendto(packet,sender_addr)
 
 
-args = parser.parse_args(argv[1:])
-
-HOST = ''
-PORT = args.port
-
-ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ss.bind((HOST, PORT))
-conn, address = ss.accept()
-
-#review class lecture for the packet sending, I believe for stop and wait we would need to send the packets line by line, and we send it to the reciever until they get it back
+if __name__ == '__main__':
+	main()
